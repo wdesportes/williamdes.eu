1a9af808f7fbfff0dfaaf510740df05ab7901977
William Desportes <williamdes@wdes.fr>
2023-05-20 23:20:43 +0200

    Update to English

--- williamdes.eu/static/export-gpg.sh
+++ williamdes.eu/static/export-gpg.sh
@@ -13,6 +13,5 @@ echo "Exported using: $CMD_GPG_EXPORT\n" > $ME/public-key.asc.txt
 echo "gpg --refresh-keys" >> $ME/public-key.asc.txt
 echo "gpg -v --check-signatures $KEY_ID" >> $ME/public-key.asc.txt
 
-$CMD_GPG_EXPORT > $ME/public-key.asc
-gpg -v --check-signatures ${KEY_ID} >> $ME/public-key.asc.txt
-# LC_ALL=C.UTF-8
+LC_ALL=C.UTF-8 $CMD_GPG_EXPORT > $ME/public-key.asc
+LC_ALL=C.UTF-8 gpg -v --check-signatures ${KEY_ID} >> $ME/public-key.asc.txt
